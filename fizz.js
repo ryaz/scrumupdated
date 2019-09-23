9560b73885eab680596a873722c1dd97412cd908
Charlie Lambropoulos <charlie101010@gmail.com>
2019-09-23 11:08:45 -0700

    vylla

---
+++ fizz.js
@@ -0,0 +1,29 @@
+function fizzbuzz(){
+
+for (var count = 1; count<100; count++) {
+
+     if (count%3==0) {
+
+      console.log(Fizz);
+
+     }
+
+     if (count%5==0) {
+
+      console.log(Buzz);
+
+     }
+
+     if (count%5==0 && count%3==0) {
+
+      console.log(FizzBuzz);
+
+     }
+     else {
+         console.log (count)           
+}
+
+}
+
+
+fizzbuzz();
